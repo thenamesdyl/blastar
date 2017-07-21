d9cecb368b46918d036b01797010d6d6305680e6
Dylan Burton <dylan@dran-mc.com>
2017-07-21 15:54:03 -0500

    Added new entryscreen

--- app/src/main/java/me/dylanburton/blastarreborn/EntryScreen.java
+++ app/src/main/java/me/dylanburton/blastarreborn/EntryScreen.java
@@ -20,6 +20,8 @@ public class EntryScreen extends Screen {
     Rect scaledDst = new Rect(); // generic rect for scaling
     Rect playBtnBounds = null;
     Rect exitBtnBounds = null;
+    private int width;
+    private int height;
 
 
     public EntryScreen(MainActivity act) {
@@ -27,7 +29,7 @@ public class EntryScreen extends Screen {
         try {
             // load screen bg
             AssetManager assetManager = act.getAssets();
-            InputStream inputStream = assetManager.open("cockpitplaceholder.jpg");
+            InputStream inputStream = assetManager.open("entryscreen.png");
             screenbtm = BitmapFactory.decodeStream(inputStream);
             inputStream.close();
 
@@ -47,28 +49,31 @@ public class EntryScreen extends Screen {
       // nothing to update
     }
 
+
+    private void drawCenteredText(Canvas c, String msg, int height, Paint p, int shift) {
+        c.drawText(msg, (width - p.measureText(msg)) / 2 + shift, height, p);
+    }
+
+
     @Override
     public void draw(Canvas c, View v) {
-        int width = v.getWidth();
-        int height = v.getHeight();
+        width = v.getWidth();
+        height = v.getHeight();
         if (playBtnBounds == null) {
             // initialize button locations
             playBtnBounds = new Rect(width/2 - playbtm.getWidth()/2,
-                    height/6 - playbtm.getHeight()/2,
+                    height/6 - playbtm.getHeight(),
                     width/2 + playbtm.getWidth()/2,
                     height/6 + playbtm.getHeight()/2);
-            exitBtnBounds = new Rect(width/2 - exitbtm.getWidth()/2,
-                    height*4/5  - exitbtm.getHeight()/2,
-                    width/2 + exitbtm.getWidth()/2,
+            exitBtnBounds = new Rect(width*85/100 - exitbtm.getWidth()/2,
+                    height*3/5  - exitbtm.getHeight()/2,
+                    width*4/5 + exitbtm.getWidth()/2,
                     height*4/5 + exitbtm.getHeight()/2);
         }
 
         // draw the screen
         scaledDst.set(0, 0, width, height);
         c.drawBitmap(screenbtm, null, scaledDst, p);
-
-        c.drawBitmap(playbtm, null, playBtnBounds, p);
-        c.drawBitmap(exitbtm, null, exitBtnBounds, p);
 
         // version/copyright line
         p.setColor(Color.rgb(0,70,0));  // dark greenish
@@ -80,6 +85,19 @@ public class EntryScreen extends Screen {
         int w1 = scaledDst.width();
         msg = "(c) 2017 Dylan Burton";
         c.drawText(msg, xTextEnd-p.measureText(msg), height - 40, p);
+        p.setColor(Color.rgb(255,55,55));
+        p.setTextSize(200);
+
+        drawCenteredText(c, "Play", height*22/100-playbtm.getHeight()/2,p,0);
+        p.setColor(Color.rgb(0,0,0));
+        p.setTextSize(70);
+        drawCenteredText(c, "About", height*78/100-playbtm.getHeight()/2,p,-width*31/100);
+        p.setTextSize(70);
+        drawCenteredText(c, "Exit", height*78/100-playbtm.getHeight()/2,p,+width*31/100);
+        p.setTextSize(300);
+        p.setColor(Color.rgb(255,255,255));
+        drawCenteredText(c, "Blastar", height*14/15,p,0);
+
     }
 
     @Override
